19cd4c1af691bffda226944e51b68008bf4554d7
daniel-boxall <danielboxall07@gmail.com>
2023-01-27 18:23:49 +0000

    first commit file set up

---
+++ script.js
@@ -0,0 +1,4 @@
+var APIKey = "1cf684ec58e0dc4383dcdd15244363d7";
+
+// Here we are building the URL we need to query the database
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
