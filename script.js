56ba6c1d9b8b6daf8eb211fcd0e3af6669171c68
daniel-boxall <danielboxall07@gmail.com>
2023-01-30 22:54:32 +0000

    got both forecasts diplaying correctly modified js

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ function showCity() {
         event.preventDefault();
         var city = $("#search-input").val().trim();
         showCityWeather(city);
-    })
+        setTimeout(200);
+        forecast(city);
+         $("#forecast").removeClass("display");
+    }) 
+    
 };
 
 function showCityWeather(city) {
@@ -29,22 +33,24 @@ function showCityWeather(city) {
         var tempK = response.main.temp;
         var tempC = parseFloat(tempK - 273.15).toFixed(1);
 
-        var today = moment().format("L");
+        var today = moment().format("l");
 
         var iconPath = response.weather[0].icon;
         var iconUrl = "https://openweathermap.org/img/wn/" + iconPath + ".png";
         var icon = $("<img>").attr("src", iconUrl);
 
-        var cityName = $("<h4 class='cityname'>").append(city, " (",  today, ")", icon);
+        var cityName = $("<h4 class='cityname'>").append(city, " (", today, ")", icon);
         var tempTag = $("<p>").text("Temp: " + tempC + " °C");
-        var windTag = $("<p>").text("Wind: " + wind + " KPH");
+        var windTag = $("<p>").text("Wind Speed: " + wind + " Mph");
         var humidityTag = $("<p>").text("Humidity: " + humidity + "%");
 
         $("#today").append(cityName, tempTag, windTag, humidityTag);
     })
-    $("#search-input").val("");
-    renderCityBtns()
-}
+        $("#search-input").val("");
+    renderCityBtns();
+    
+    }
+
 
 function renderCityBtns() {
     $("#history").empty();
@@ -60,160 +66,103 @@ function renderCityBtns() {
     $(".cityBtn").on("click", function (event) {
         var city = $(this).text();
         console.log(city);
-      showCityWeather(city);
-      forecast(city);
+        showCityWeather(city);
+        forecast(city);
+        $("#forecast").removeClass("display");
     })
 }
 renderCityBtns();
 showCity();
 
-function forecast(city) {
-  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-    console.log(queryURL)
-    console.log(response)
-    //DAY 1
-    var iconCode = response.list[8].weather[0].icon;
-    var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
-    $(".weather1").attr("src", iconURL);
+ function forecast(city) {
+  
+   var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+ 
+   $.ajax({
+     url: queryURL,
+     method: "GET"
+   }).then(function (response) {
+     console.log(queryURL)
+     console.log(response)
 
-    var tempC = Math.floor(response.list[8].main.temp - 273.15);
-    var wind = response.list[8].wind.speed;
-    var humidity = response.list[8].main.humidity;
-    $("#image").append(iconURL);
-    //append data to webpage
-    $("#temp-day1").append("Temp: " + tempC + "°C")
-    $("#wind-day1").append("Wind Speed: " + wind + "MPH")
-    $("#humidity-day1").append("Humidity: " + humidity + "%")
+     iconCode = response.list[8].weather[0].icon;
+     iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+     $(".weather1").attr("src", iconURL);
+       
+     var tempC = Math.floor(response.list[8].main.temp - 273.15);
+     var wind = response.list[8].wind.speed;
+     var humidity = response.list[8].main.humidity;
+     //$("day1").append(todayDay) + 1;
+     $("#image").append(iconURL);
+      //append data to webpage
+     $("#day1").text(moment.unix(response.list[8].dt).format("DD/MM/YY"));
+     $("#temp-day1").text("Temp: " + tempC + "°C")
+     $("#wind-day1").text("Wind Speed: " + wind + " Mph")
+     $("#humidity-day1").text("Humidity: " + humidity + "%")
 
     // DAY 2
-    iconcode = response.list[16].weather[0].icon;
-    iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
-    $(".weather2").attr("src", iconURL);
+     iconCode = response.list[16].weather[0].icon;
+     iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+     $(".weather2").attr("src", iconURL);
 
-    var tempC = Math.floor(response.list[16].main.temp - 273.15);
-    var wind = response.list[16].wind.speed;
-    var humidity = response.list[16].main.humidity;
-    $("#image").append(iconURL);
-    //append data to webpage
-    $("#temp-day2").append("Temp: " + tempC + "°C")
-    $("#wind-day2").append("Wind Speed: " + wind + "MPH")
-    $("#humidity-day2").append("Humidity: " + humidity + "%")
+     var tempC = Math.floor(response.list[16].main.temp - 273.15);
+     var wind = response.list[16].wind.speed;
+     var humidity = response.list[16].main.humidity;
+     $("#image").append(iconURL);
+     //append data to webpage
+     $("#day2").text(moment.unix(response.list[16].dt).format("DD/MM/YY"));
+     $("#temp-day2").text("Temp: " + tempC + "°C")
+     $("#wind-day2").text("Wind Speed: " + wind + "Mph")
+     $("#humidity-day2").text("Humidity: " + humidity + "%")
 
-    // DAY 3
-    iconcode = response.list[24].weather[0].icon;
-    iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
-    $(".weather3").attr("src", iconURL);
+     // DAY 3
+     iconCode = response.list[24].weather[0].icon;
+     iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+     $(".weather3").attr("src", iconURL);
 
-    var tempC = Math.floor(response.list[24].main.temp - 273.15);
-    var wind = response.list[24].wind.speed;
-    var humidity = response.list[24].main.humidity;
-    $("#image").append(iconURL);
-    //append data to webpage
-    $("#temp-day3").append("Temp: " + tempC + "°C")
-    $("#wind-day3").append("Wind Speed: " + wind + "MPH")
-    $("#humidity-day3").append("Humidity: " + humidity + "%")
+     var tempC = Math.floor(response.list[24].main.temp - 273.15);
+     var wind = response.list[24].wind.speed;
+     var humidity = response.list[24].main.humidity;
+     $("#image").append(iconURL);
+     //append data to webpage
+     $("#day3").text(moment.unix(response.list[24].dt).format("DD/MM/YY"));
+     $("#temp-day3").text("Temp: " + tempC + "°C")
+     $("#wind-day3").text("Wind Speed: " + wind + "Mph")
+     $("#humidity-day3").text("Humidity: " + humidity + "%")
 
-    // DAY 4
-    iconcode = response.list[32].weather[0].icon;
-    iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
-    $(".weather4").attr("src", iconURL);
+     // DAY 4
+     iconCode = response.list[32].weather[0].icon;
+     iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+     $(".weather4").attr("src", iconURL);
 
-    var tempC = Math.floor(response.list[32].main.temp - 273.15);
-    var wind = response.list[32].wind.speed;
-    var humidity = response.list[32].main.humidity;
-    $("#image").append(iconURL);
-    //append data to webpage
-    $("#temp-day4").append("Temp: " + tempC + "°C")
-    $("#wind-day4").append("Wind Speed: " + wind + "MPH")
-    $("#humidity-day4").append("Humidity: " + humidity + "%")
+     var tempC = Math.floor(response.list[32].main.temp - 273.15);
+     var wind = response.list[32].wind.speed;
+     var humidity = response.list[32].main.humidity;
+     $("#image").append(iconURL);
+     //append data to webpage
+     $("#day4").text(moment.unix(response.list[32].dt).format("DD/MM/YY"));
+     $("#temp-day4").text("Temp: " + tempC + "°C")
+     $("#wind-day4").text("Wind Speed: " + wind + "Mph")
+     $("#humidity-day4").text("Humidity: " + humidity + "%")
 
-    // DAY 5
-    iconcode = response.list[39].weather[0].icon;
-    iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
-    $(".weather5").attr("src", iconURL);
+     // DAY 5
+     iconCode = response.list[39].weather[0].icon;
+     iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+     $(".weather5").attr("src", iconURL);
 
-    var tempC = Math.floor(response.list[39].main.temp - 273.15);
-    var wind = response.list[39].wind.speed;
-    var humidity = response.list[39].main.humidity;
-    $("#image").append(iconURL);
-    //append data to webpage
-    $("#temp-day5").append("Temp: " + tempC + "°C")
-    $("#wind-day5").append("Wind Speed: " + wind + "MPH")
-    $("#humidity-day5").append("Humidity: " + humidity + "%")
+     var tempC = Math.floor(response.list[39].main.temp - 273.15);
+     var wind = response.list[39].wind.speed;
+     var humidity = response.list[39].main.humidity;
+     $("#image").append(iconURL);
+     //append data to webpage
 
+     $("#day5").text(moment.unix(response.list[39].dt).format("DD/MM/YY"));
+     $("#temp-day5").text("Temp: " + tempC + "°C")
+     $("#wind-day5").text("Wind Speed: " + wind + "Mph")
+     $("#humidity-day5").text("Humidity: " + humidity + "%")
 
-  });
-  
-  
-  
-};
-
-
-
-
-
-
-/*var 
-var long = "";
-var lat = "";
-
-$("#search-button").on("click", function (event) {
-    event.preventDefault();
-    // This line of code will grab the input from the textbox
-    var search = $("#search-input").val();
-
-    var query1 = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=" + APIKey;
-
-  $.ajax({
-    url: query1,
-    method: "GET"
-  }).then(function (e) {
-
-    // Create CODE HERE to Log the queryURL
-    console.log(query1);
-    // Create CODE HERE to log the resulting object
-    console.log(e);
-        
-    long = e.coord.lon;
-    lat = e.coord.lat;
-    console.log(long)
-    console.log(lat)
-
-  
-      var query2 = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=" + APIKey + "&units=metric";
-    
-      $.ajax({
-          url: query2,
-          method: "GET"
-      }).then(function (response) {
-
-      // Create CODE HERE to Log the queryURL
-      console.log(query2);
-      // Create CODE HERE to log the resulting object
-      console.log(response);
-      });
-        let cityEl = $("<div>")
-        let tempEl = $("<div>")
-        let windEl = $("<div>")
-        let humidityEl = $("<div>")
-  
-        cityEl.html(response.name)
-        tempEl.html(response.main.temp + " C")
-        windEl.html(response.wind.speed + " Mph")
-        humidityEl.html(response.main.humidity + " % humidity")
-
-        $("#today").append(cityEl, tempEl, windEl, humidityEl);
-  
-    });
-  });
-  
-  */
-  
- 
-  
+   });
+ }
+     
 
